9ffa058446459ca75dd6a1160c3fa19cf226d140
maymunah03 <maymunahhicks786@gmail.com>
2024-07-22 10:43:13 -0500

    Added new APIs and functions

--- week-3/simple-web-server/server.js
+++ week-3/simple-web-server/server.js
@@ -1,17 +1,24 @@
-const express = require('express'); // Import the express library
-const path = require('path'); // Import the path library
-const app = express(); // Create an express app
-const PORT = 3000; // Set the port
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Simple API endpoint
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+// Simple API endpoints
+app.get('/api/user', (req, res) => {
+    res.json({ Name: 'May Hicks, Alloy Intern'});
 });
 
+app.get('/api/status', (req, res) => {
+    res.json({Status: 'You have now been signed in.'});
+});
+
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Have a great day, May!' });
+  });
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log('Server is running on http://localhost:${PORT}');
 });
