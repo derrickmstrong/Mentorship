d00f7d049573154eca87d53f260e6955c3e47445
Derrick Strong <derrick.strong@alloy.digital>
2024-07-22 12:10:17 -0500

    Update server.js to use template literals for console.log message

--- week-3/simple-web-server/server.js
+++ week-3/simple-web-server/server.js
@@ -20,5 +20,5 @@ app.get('/api/message', (req, res) => {
   });
 // Start the server
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:${PORT}');
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
